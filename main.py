508623fd376f7a676b4fbb28b795492173e3fb93
Ansar <Tiplar85@gmail.com>
2021-12-04 10:39:00 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic, QtGui, QtWidgets
+from PyQt5.QtGui import QPainter, QColor
+import random
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('lolo.ui', self)
+        self.should_paint_circle = False
+        self.lolo = QtWidgets.QPushButton('НАЖАТЬ', self)
+        self.lolo.setGeometry(10, 10, 200, 100)
+
+        self.lolo.clicked.connect(self.paintcircle)
+        self.lolo.clicked.connect(self.paintEvent)
+        self.should_paint_circle = False
+
+    def paintEvent(self, event):
+        if self.should_paint_circle:
+            painter = QtGui.QPainter(self)
+            painter.setBrush(QColor('YELLOW'))
+            pen = QtGui.QPen()
+            pen.setWidth(5)
+            painter.setPen(pen)
+            a = random.randint(1, 500)
+            painter.drawEllipse(300, 300, a, a)
+
+    def paintcircle(self):
+        self.should_paint_circle = True
+        self.update()
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    application()
+
